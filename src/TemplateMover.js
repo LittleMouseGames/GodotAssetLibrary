301c19d2f5b04b69e2896ee4f187d907baedd91e
Chris Bolton <hello@chrisbolton.dev>
2022-02-05 22:17:05 -0500

    Watch template changes and move 'em

--- src/TemplateMover.js
+++ src/TemplateMover.js
@@ -3,13 +3,13 @@ const path = require('path')
 
 async function moveTemplates () {
   await moveComponents()
-  await movePages()
-}
 
-/**
-  * Moves pages to a 'nicer' namespace in dist/
-*/
-async function movePages () {
+  fs.watch(path.join(__dirname, '/backend/components'), (eventType) => {
+    if (eventType === 'change') {
+      moveComponents()
+    }
+  })
+
   const route = path.join(__dirname, '/backend/modules/pages')
 
   const directories = source => fs.readdirSync(source, {
@@ -23,16 +23,28 @@ async function movePages () {
 
   pageModules.forEach(async function (module) {
     if (fs.existsSync(path.join(route, module, 'views'))) {
-      await fs.copy(path.join(route, module, 'views'), path.join(__dirname, '../dist/pages/', module))
+      await movePages(route, module)
+
+      fs.watch(path.join(route, module, 'views'), async function (eventType) {
+        if (eventType === 'change') {
+          await movePages(route, module)
+        }
+      })
     }
   })
+}
+
+/**
+  * Moves pages to a 'nicer' namespace in dist/
+*/
+async function movePages (route, module) {
+  await fs.copy(path.join(route, module, 'views'), path.join(__dirname, '../dist/pages/', module))
 }
 
 /**
   * Moves all components to dist/ folder
 */
 async function moveComponents () {
-  console.log(__filename)
   await fs.copy(path.join(__dirname, '/backend/components'), path.join(__dirname, '../dist/components'))
 }
 
