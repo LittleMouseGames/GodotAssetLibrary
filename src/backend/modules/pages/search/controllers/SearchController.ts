158f7adf94d4f73570060aac726b59186362e516
Chris Bolton <hello@chrisbolton.dev>
2022-02-13 03:52:18 -0500

    Remove apicache module

--- src/backend/modules/pages/search/controllers/SearchController.ts
+++ src/backend/modules/pages/search/controllers/SearchController.ts
@@ -1,9 +1,6 @@
-import { Controller, Get, Middleware, Post } from '@overnightjs/core'
+import { Controller, Get, Post } from '@overnightjs/core'
 import { Request, Response } from 'express'
 import { SearchService } from '../services/SearchService'
-import apicache from 'apicache'
-
-const cache = apicache.middleware
 
 /**
  * The user controller
@@ -16,7 +13,6 @@ export class SearchController {
    * Home
    */
   @Get('/')
-  @Middleware([cache('5 minutes')])
   private async index (req: Request, res: Response): Promise<void> {
     return await this.SearchService.render(req, res)
   }
