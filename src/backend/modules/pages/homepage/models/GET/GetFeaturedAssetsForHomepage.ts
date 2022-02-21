c619995f29e70ab009c4e28107a1f8b6b540d4a3
Chris Bolton <hello@chrisbolton.dev>
2022-02-20 19:09:01 -0500

    Add featured assets to homepage

---
+++ src/backend/modules/pages/homepage/models/GET/GetFeaturedAssetsForHomepage.ts
@@ -0,0 +1,39 @@
+import { Document, WithId } from 'mongodb'
+import { MongoHelper } from 'MongoHelper'
+import { assetGridSchema } from 'utility/schema/assets-grid'
+
+interface ReturnedAssets extends WithId<Document>, assetGridSchema {}
+
+export async function GetFourAssetsForHomepage (): Promise<ReturnedAssets[]> {
+  const mongo = MongoHelper.getDatabase()
+  const operationObject = await mongo.collection('assets').aggregate([{
+    $match: {
+      featured: true
+    }
+  }, {
+    $sample: {
+      size: 3
+    }
+  }, {
+    $project: {
+      category: 1,
+      godot_version: 1,
+      author: 1,
+      title: 1,
+      quick_description: 1,
+      icon_url: 1,
+      upvotes: 1,
+      downvotes: 1,
+      featured: 1,
+      asset_id: 1,
+      previews: 1,
+      card_banner: 1
+    }
+  }]).toArray() as ReturnedAssets[]
+
+  if (operationObject === null || operationObject === undefined) {
+    throw new Error('No assets found')
+  }
+
+  return operationObject
+}
--- src/backend/modules/pages/homepage/services/HomepageService.ts
+++ src/backend/modules/pages/homepage/services/HomepageService.ts
@@ -1,13 +1,15 @@
 import { Request, Response } from 'express'
 import { logger } from 'utility/logger'
+import { GetFourAssetsForHomepage } from '../models/GET/GetFeaturedAssetsForHomepage'
 import { GetFourAssets } from '../models/GET/GetFourAssets'
 
 export class HomepageService {
   public async render (_req: Request, res: Response): Promise<void> {
     try {
       const assets = await GetFourAssets()
+      const featuredAssets = await GetFourAssetsForHomepage()
 
-      return res.render('templates/pages/homepage/index', { assets: assets })
+      return res.render('templates/pages/homepage/index', { assets: assets, featuredAssets: featuredAssets })
     } catch (e: any) {
       logger.log('error', 'Failed to retrieve assets', ...[e])
       return res.render('templates/pages/homepage/index')
