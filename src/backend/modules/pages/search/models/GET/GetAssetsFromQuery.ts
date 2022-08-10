087b038bdfa6ff14baf72b75add081c4c249e4b7
Chris Bolton <hello@chrisbolton.dev>
2022-08-09 23:52:00 -0400

    Remove debug statement

--- src/backend/modules/pages/search/models/GET/GetAssetsFromQuery.ts
+++ src/backend/modules/pages/search/models/GET/GetAssetsFromQuery.ts
@@ -14,8 +14,6 @@ export async function GetAssetsFromQuery (query: string, limit: number = 12, ski
     }
   }
 
-  console.log(categoryFilters)
-
   if (Array.isArray(engineFilters) && engineFilters.length > 0) {
     filters.godot_version = {
       $in: engineFilters
