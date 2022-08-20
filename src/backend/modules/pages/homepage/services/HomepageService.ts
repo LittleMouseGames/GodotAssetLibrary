4739eb5df47b72350241b555101d35ae87771173
Chris Bolton <hello@chrisbolton.dev>
2022-08-19 22:45:46 -0400

    Remove Latest Imported homepage section

--- src/backend/modules/pages/homepage/services/HomepageService.ts
+++ src/backend/modules/pages/homepage/services/HomepageService.ts
@@ -6,7 +6,6 @@ import { logger } from 'utility/logger'
 import { GetAllCategoriesAndTheirAssetCount } from '../models/GET/GetAllCategoriesAndTheirAssetCount'
 import { GetFourAssetsForHomepage } from '../models/GET/GetFeaturedAssetsForHomepage'
 import { GetLastModifiedAssets } from '../models/GET/GetLastModifiedAssets'
-import { GetNewestAssets } from '../models/GET/GetNewestAssets'
 import { GetTrendingAssets } from '../models/GET/GetTrendingAssets'
 
 export class HomepageService {
@@ -15,14 +14,13 @@ export class HomepageService {
     try {
       const trendingAssets = await GetTrendingAssets()
       const featuredAssets = await GetFourAssetsForHomepage()
-      const newestAssets = await GetNewestAssets()
       const lastModifiedAssets = await GetLastModifiedAssets()
       const categoriesObject = await GetAllCategoriesAndTheirAssetCount()
 
       if (authToken !== '') {
         const tokenServices = TokenServices.getInstance()
         const hashedToken = tokenServices.hashToken(authToken)
-        const assetPointers = [trendingAssets, featuredAssets, newestAssets, lastModifiedAssets]
+        const assetPointers = [trendingAssets, featuredAssets, lastModifiedAssets]
 
         try {
           const userSaved = await GetUserSavedAssets(hashedToken)
@@ -36,7 +34,7 @@ export class HomepageService {
         }
       }
 
-      return res.render('templates/pages/homepage/index', { trendingAssets: trendingAssets, featuredAssets: featuredAssets, newestAssets: newestAssets, lastModifiedAssets: lastModifiedAssets, categoriesObject: categoriesObject })
+      return res.render('templates/pages/homepage/index', { trendingAssets: trendingAssets, featuredAssets: featuredAssets, lastModifiedAssets: lastModifiedAssets, categoriesObject: categoriesObject })
     } catch (e: any) {
       logger.log('error', 'Failed to retrieve assets', ...[e])
       return res.render('templates/pages/homepage/index')
